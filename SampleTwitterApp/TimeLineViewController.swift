e5d638ba11a821ca125036087ee3ce3866a0412f
kosuke <cheki3cheki3@gmail.com>
2021-10-19 22:24:10 +0900

    ツイート機能作成中2

--- SampleTwitterApp/TimeLineViewController.swift
+++ SampleTwitterApp/TimeLineViewController.swift
@@ -7,66 +7,52 @@
 
 import UIKit
 
-class TimeLineViewController: UIViewController {
+struct Item {
+    var icon: UIImage
+    var name: String
+    var tweet: String
+}
 
-
-    @IBOutlet weak var tableView: UITableView!
-    // テーブル表示用のデータソース
-    var tweets: [Tweet] = []
+class TimeLineViewController: UITableViewController {
+    
+//    ここにアイコン、名前、tweetの配列を入れる
+    private let items: [Item] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView.delegate = self
-        // dataSourceの指定を自分自身(self = TimelineViewController)に設定
-        tableView.dataSource = self
-        // ダミーデータの生成
-        let user = User(id: "1", screenName: "ktanaka117", name: "ダンボー田中", profileImageURL: "https://pbs.twimg.com/profile_images/832034247414206464/PCKoQRPD.jpg")
-        let tweet = Tweet(id: "01", text: "Twitterクライアント作成なう", user: user)
-
-        let tweets = [tweet]
-        self.tweets = tweets
-
-        // tableViewのリロード
-        tableView.reloadData()
-        
-
             }
     
-        }
-extension TimeLineViewController: UITableViewDelegate {
+        
     // cellがタップされたのを検知したときに実行する処理
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("セルがタップされました！")
     }
     // セルの見積もりの高さを指定する処理
-    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         
         return 200
     }
     // セルの高さ指定をする処理
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         // UITableViewCellの高さを自動で取得する値
         return UITableView.automaticDimension
     }
-}
 
-extension TimeLineViewController: UITableViewDataSource {
+
     
 //    何個のセルを生成するか？
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // tweetsの配列内の要素数分を指定
-        return tweets.count
+        items.count
     }
     
 //    描画するセルを指定する
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // TweetTableViewCellを表示したいので、TweetTableViewCellを取得
         let cell = tableView.dequeueReusableCell(withIdentifier: "TweetTableViewCell") as! TweetTableViewCell
         
         // TweetTableViewCellの描画内容となるtweetを渡す
-        cell.fill(tweet: tweets[indexPath.row])
-        
+        cell.configure(item: items[indexPath.row])
         return cell
     }
     
--- SampleTwitterApp/TweetTableViewCell.swift
+++ SampleTwitterApp/TweetTableViewCell.swift
@@ -6,33 +6,24 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseStorage
+import FirebaseAuth
 
 class TweetTableViewCell: UITableViewCell{
-    @IBOutlet weak var iconImageView: UIImageView!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var textContentLabel: UILabel!
 
-    func fill(tweet: Tweet) {
-        // profileImageURLから画像をダウンロードしてくる処理
-        let downloadTask = URLSession.shared.dataTask(with: URL(string: tweet.user.profileImageURL)!) { [weak self] data, response, error in
-            if let error = error {
-                print(error)
-                return
-            }
-
-            DispatchQueue.main.async {
-                // iconImageViewにダウンロードしてきた画像を代入する処理
-                self?.iconImageView.image = UIImage(data: data!)
-            }
+    @IBOutlet private weak var iconImageView: UIImageView!
+    @IBOutlet private weak var userNameLabel: UILabel!
+    @IBOutlet private weak var tweetLabel: UILabel!
+    
+    func configure(item: Item) {
+        iconImageView.image = item.icon
+        userNameLabel.text = item.name
+        tweetLabel.text = item.tweet
         }
-        downloadTask.resume()
+            }
 
-        // tweetから値を取り出して、UIにセットする
-        nameLabel.text = tweet.user.name
-        textContentLabel.text = tweet.text
-        // screenNameには "@" が含まれていないので、頭に "@" を入れてあげる必要がある
-        screenNameLabel.text = "@" + tweet.user.screenName
-    }
-}
+
+
 
 
